71d1ed3764103c7442b4fbc0318bbdd0d5971aa9
Dustin Schau <dustinschau@gmail.com>
2022-07-12 14:38:14 -0700

    chore: add ssr homepage

---
+++ src/pages/homepage.js
@@ -0,0 +1,55 @@
+import * as React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+import * as sections from "../components/sections"
+import Fallback from "../components/fallback"
+
+export default function Homepage(props) {
+  const { homepage } = props.data
+
+  return (
+    <Layout {...homepage}>
+      {homepage.blocks.map((block) => {
+        const { id, blocktype, ...componentProps } = block
+        const Component = sections[blocktype] || Fallback
+        return <Component key={id} {...componentProps} />
+      })}
+    </Layout>
+  )
+}
+
+/*
+ * Note: this doesn't do anything
+ * but it mimics an SSR call to ensure that we are doing SSR
+ */
+export function getServerData() {
+  return {
+    props: {}
+  }
+}
+
+export const query = graphql`
+  {
+    homepage {
+      id
+      title
+      description
+      image {
+        id
+        url
+      }
+      blocks: content {
+        id
+        blocktype
+        ...HomepageHeroContent
+        ...HomepageFeatureListContent
+        ...HomepageCtaContent
+        ...HomepageLogoListContent
+        ...HomepageTestimonialListContent
+        ...HomepageBenefitListContent
+        ...HomepageStatListContent
+        ...HomepageProductListContent
+      }
+    }
+  }
+`
